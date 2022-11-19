e115de41dee9b958daae710d2d243a34f2cb1c50
DarkHouse123 <ivokarapeev@gmail.com>
2022-11-19 09:22:05 +0200

    Creating details button for products

--- client/src/components/CatalogClothes/CatalogItem.js
+++ client/src/components/CatalogClothes/CatalogItem.js
@@ -1,4 +1,5 @@
 import styles from './CatalogClothes.module.css';
+import { Link } from 'react-router-dom';
 
 const CatalogItem = ({item}) => {
     return (
@@ -14,6 +15,7 @@ const CatalogItem = ({item}) => {
                     {item.name}
                     <br />
                     <span>{item.category}</span>
+                    <button className={styles["details-btn"]}><Link to={`/product/details/${item._id}`}>Details</Link></button>
                     </h2>
                 </div>
             </div>
