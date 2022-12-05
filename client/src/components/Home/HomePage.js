1764f2d13f16d44a598a3740c4c99fb07c25f18e
DarkHouse123 <ivokarapeev@gmail.com>
2022-12-05 10:19:22 +0200

    Changing HomePage css

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -6,25 +6,25 @@ const HomePage = () => {
     const navigate = useNavigate();
 
     const onClick = (to) => {
+
         if (to === 'clothes') {
-            navigate('/catalog/clothes');
-        }else if (to === 'shoes') {
-            navigate('/catalog/shoes');
-        }else if (to === 'accessories') {
-            navigate('/catalog/accessories');
-        }else{
-            navigate('/procuts/sell');
-        }
-    };
+            navigate('/catalog/clothes');        
+        } else if (to === 'shoes') {
+            navigate('/catalog/shoes');        
+        } else {
+            navigate('/catalog/accessories');        
+        };
+
+    }
 
     return (
         <div>
-            <div className={styles['page-image']}>
-            </div>
-            <button className={styles['btn-clothes']} onClick={() => onClick('clothes')}>You want to buy clothes that are exactly in your style, click here</button>
-            <button className={styles['btn-shoes']} onClick={() => onClick('shoes')}>You want to buy shoes with which you feel comfortable, click here</button>
-            <button className={styles['btn-accessories']} onClick={() => onClick('accessories')}>You want to buy accessories for you or your loved one, click here</button>
-            <button className={styles['btn-sell']} onClick={() => onClick('sell')}>You want to sell your item, click here</button>
+            <button className={styles['btn-clothes']} onClick={() => onClick('clothes')}></button>
+            <p className={styles.clothes}>Clothes</p>
+            <button className={styles['btn-shoes']} onClick={() => onClick('shoes')}></button>
+            <p className={styles.shoes}>Shoes</p>
+            <button className={styles['btn-accessories']} onClick={() => onClick('accessories')}></button>
+            <p className={styles.accessories}>Accessories</p>
         </div>
     )
 
